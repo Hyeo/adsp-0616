ff346e0a3e2d1aa9141e564cfe51e597516f348f
Macro <macro0630@gmail.com>
2020-06-16 22:21:34 +0900

    a

--- part01/03_ex_variables.R
+++ part01/03_ex_variables.R
@@ -1,4 +1,41 @@
 # https://docs.google.com/presentation/d/1vAFR3Gut5Kc4rS_D6qTZg1oasTEoPTuXBCKgt01BJ68/edit#slide=id.g7e8cc96aa9_0_27
 
+# 1. 
+
+a <- 11
+b <- 7
+
+Area <- a * b
+Area
+
+Perimeter <- 2 * ( a + b)
+Perimeter
+
+a <- 22
+
+# 2.
+
+x <- 12
+x %% 2
+
+# 3. 
+
+a <- 20
+
+area <- 6 * a ^ 2
+area
+
+volume <- a ^ 3
+volume
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ part01/operators_variables.R
@@ -0,0 +1,78 @@
+2 + 3 
+
+12.2  +     13.5
+
+5 -   10
+
+1 / 2
+
+4 ^ 8
+
+4 ** 8
+
+4 * * 8
+
+7 %% 3
+
+7 %/% 2
+
+(100 + 2) * 50 / (9 %%4)
+
+# 변수  (Variables)
+
+age <- 30
+word <- '고등어'
+word2 <- "갈치"
+
+3word <- "꽁치"
+
+word4 <- 'hello'
+
+age + 30
+
+score + 20
+
+kor_score <- 50
+eng_score <- 100
+
+kor_score <- kor_score + 20
+
+bank account <- 1000000
+
+bank.account <- 1000000
+
+deposit <- 10000
+
+bank.account <- bank.account - deposit
+
+n <- 2.2
+i <- 5
+
+t <- TRUE
+f <- FALSE
+
+sentence <- 'Hello World!'
+
+class(n)
+class(i)
+class(t)
+class(f)
+class(sentence)
+
+is.numeric(n)
+is.integer(n)
+is.integer(i)
+
+is.double(n)
+is.double(i)
+
+int <- 5L
+is.double(int)
+is.integer(int)
+
+
+
+
+
+  
+  
